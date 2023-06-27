63bcb730a1c65975697032e09429beba0373ef73
connorGA <ronnoccap@gmail.com>
2023-06-26 17:42:11 -0700

    Rubric

---
+++ 1_Python_Programming_and_Data_Manipulation.py
@@ -0,0 +1,47 @@
+# 1. PYTHON FUNDAMENTALS --------------------------------------------------------------------------
+    #Syntax: Understand how Python's indentation works, how to write comments, and the rules for naming variables.
+
+    #Data Types and Structures: Know the characteristics and use cases of different data types (e.g., int, float, bool, str) and data structures (e.g., list, tuple, dictionary, set). Understand how to perform common operations like indexing, slicing, appending, and deleting elements.
+
+    #Control Flow: Be comfortable with if-else statements, for and while loops, break and continue statements, and exception handling (try-except blocks).
+
+    #Functions: Understand how to define and invoke functions, the difference between return and print, and how to use default and keyword arguments.
+
+    #Modules and Packages: Understand how to import a module or package, and the difference between import, from-import, and import as statements.
+
+# 2. ADVANCED PYTHON CONCEPTS --------------------------------------------------------------------------
+    #List Comprehensions: Understand the syntax of list comprehension and how it can be used to create lists in a more pythonic way.
+
+    #Generators: Understand the concept of generators and how they are used to create iterables.
+
+    #Decorators: Know what a decorator is, how to write one, and what it can be used for.
+
+    #Context Managers: Understand what the with statement does, and how to create your own context manager using the contextlib module.
+
+    #Error Handling: Be familiar with raising exceptions, catching exceptions, the else clause, the finally clause, and creating custom exceptions.
+
+# 3. PYTHON STANDARD LIBRARY --------------------------------------------------------------------------
+    #File Operations: Be familiar with opening, reading, writing, and closing files in Python, as well as handling file paths.
+
+    #JSON/XML parsing: Understand how to parse and manipulate JSON or XML data.
+
+    #Datetime Module: Understand how to work with dates and times, including creating datetime objects, formatting and parsing datetime strings.
+
+    #Collections Module: Be familiar with the collections module and its classes like Counter, defaultdict, OrderedDict, namedtuple, and deque.
+
+# 4. DATA MANIPULATION IN PYTHON --------------------------------------------------------------------------
+    #Pandas: Understand the Pandas DataFrame and Series data types, and be comfortable with tasks like loading data, data cleaning, data wrangling, grouping and sorting data, merging dataframes, and applying functions to data.
+
+    #Numpy: Understand Numpy n-dimensional arrays, and how to perform operations like indexing, slicing, reshaping, and broadcasting. Also understand Numpy's mathematical and statistical functions.
+
+# 5. TESTING IN PYTHON --------------------------------------------------------------------------
+    #Pytest: Know how to write unit tests using pytest or unittest module, understand fixtures and mocking in testing.
+
+    #Test-Driven Development: Understand the concept of writing tests before writing the code itself, and how it can improve the design and reliability of your code.
+
+# 6. BEST PRACTICES IN PYTHON PROGRAMMING --------------------------------------------------------------------------
+    #PEP 8: Understand the PEP 8 style guide for Python code, including naming conventions, formatting guidelines, and other best practices.
+
+    #Code Readability: Understand how to write clean, concise, and readable code, and why this is important.
+
+    #Documentation: Be familiar with writing docstrings for your functions, classes, and modules, and why this is important.
